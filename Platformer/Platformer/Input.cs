00e3936f98e776d3e02f51114c83038dcbb1a291
maxim0505 <maxim.lukinykh05@gmail.com>
2019-01-20 14:49:38 +0500

    Created the "Input" class to handle input

---
+++ Platformer/Platformer/Input.cs
@@ -0,0 +1,42 @@
+﻿namespace Platformer
+{
+    static class Input
+    {
+        public static sbyte Horizontal = 0; // the horizontal axis
+        public static sbyte Vertical   = 0; // the Vertical axis
+
+        public static void PressedHandler(SFML.Window.Keyboard.Key key) // handler for pressing keys
+        {
+          
+                switch (key)
+                {
+                    case SFML.Window.Keyboard.Key.Left:  // if left key is pressed
+                        Horizontal = -1;
+                        break;
+                    case SFML.Window.Keyboard.Key.Right: // if right key is pressed
+                        Horizontal =  1;
+                        break;
+                    case SFML.Window.Keyboard.Key.Up:    // if Up key is pressed
+                        Vertical   = -1;
+                        break;
+                    case SFML.Window.Keyboard.Key.Down:  // if down key is pressed
+                        Vertical   =  1;
+                        break;
+                }
+            //Console.WriteLine("Key pressed: " + key.ToString());
+            
+        }
+        public static void ReleaseHandler(SFML.Window.Keyboard.Key key) // key release handler
+        {
+            if (key == SFML.Window.Keyboard.Key.Left && Horizontal < 0 
+                || key == SFML.Window.Keyboard.Key.Right && Horizontal > 0) // if left and right keys are released
+                Horizontal = 0; // put the horizontal axis in its original position
+
+            if (key == SFML.Window.Keyboard.Key.Up && Vertical < 0 
+                || key == SFML.Window.Keyboard.Key.Down && Vertical > 0) // if up and down keys are released
+                Vertical   = 0; // put the vertical axis in its original position
+
+            //Console.WriteLine("key released: " + key.ToString());
+        }
+    }
+}
--- Platformer/Platformer/Program.cs
+++ Platformer/Platformer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Platformer
 {
@@ -11,23 +7,27 @@ namespace Platformer
         static void Main(string[] args)
         {
             SFML.Graphics.RenderWindow window = 
-                new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(1024, 768), "Platformer"); // create window
-
-            window.Closed += Window_Closed; // connecting a window close listener
-            window.KeyPressed += Window_KeyPressed; // connecting a listener to a keystroke event
+            new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(1024, 768), "Platformer"); // create window
+           
+ 
+            window.Closed += Window_Closed;           // connecting a window close listener
+            window.KeyPressed += Window_KeyPressed;   // connecting a listener to a keystroke event
             window.KeyReleased += Window_KeyReleased; // connecting a listener to a key release event
-
+           
             while (window.IsOpen) // main loop
             {
                 window.DispatchEvents();  // sending window events
                 System.Threading.Thread.Sleep(1); // to stop the thread of 1 millisecond, to reduce the load on the processor
+                
             }
+
             window.Dispose(); //remove the window
         }
 
         private static void Window_KeyReleased(object sender, SFML.Window.KeyEventArgs e)
         {
-            throw new NotImplementedException();
+            Input.ReleaseHandler(e.Code); // transfer to the handler the release keys in a static class Input
+
         }
 
         private static void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
@@ -35,9 +35,9 @@ namespace Platformer
             SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender; // the conversion of the sender of the event in the window class
 
             if (e.Code == SFML.Window.Keyboard.Key.Escape) // close the window if the Escape key is pressed
-            {
                 window.Close(); // close the window
-            }
+            else
+                Input.PressedHandler(e.Code); // the transfer handler for the Pressed keys in a static class Input
         }
 
         private static void Window_Closed(object sender, EventArgs e) // handling a window close event 
@@ -45,5 +45,6 @@ namespace Platformer
             SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender; // the conversion of the sender of the event in the window class
             window.Close(); // close the window
         }
+     
     }
 }
