ab00f7c602bc4501b46fdac73b8ad1777ba44041
maxim0505 <maxim.lukinykh05@gmail.com>
2019-01-20 13:29:51 +0500

    window and main loop created

--- Platformer/Platformer/Program.cs
+++ Platformer/Platformer/Program.cs
@@ -10,6 +10,18 @@ namespace Platformer
     {
         static void Main(string[] args)
         {
+            SFML.Graphics.RenderWindow window = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(1024, 768), "Platformer"); // create window
+            window.Closed += Window_Closed; // connecting a window close listener
+            while (window.IsOpen) // main loop
+            {
+                window.DispatchEvents();  // sending window events
+            }
+        }
+
+        private static void Window_Closed(object sender, EventArgs e) // handling a window close event 
+        {
+            SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender; // the conversion of the sender of the event in the window class
+            window.Close(); // close the window
         }
     }
 }
