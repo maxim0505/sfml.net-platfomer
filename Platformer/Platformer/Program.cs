bb04cb36809a3b35cb4153cb2dc4b1f2cabf21af
maxim0505 <maxim.lukinykh05@gmail.com>
2019-01-20 13:49:37 +0500

    closing a window with the keyboard
    closing a window with the keyboard, keyboard pressed and release handlers

--- Platformer/Platformer/Program.cs
+++ Platformer/Platformer/Program.cs
@@ -10,11 +10,33 @@ namespace Platformer
     {
         static void Main(string[] args)
         {
-            SFML.Graphics.RenderWindow window = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(1024, 768), "Platformer"); // create window
+            SFML.Graphics.RenderWindow window = 
+                new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(1024, 768), "Platformer"); // create window
+
             window.Closed += Window_Closed; // connecting a window close listener
+            window.KeyPressed += Window_KeyPressed; // connecting a listener to a keystroke event
+            window.KeyReleased += Window_KeyReleased; // connecting a listener to a key release event
+
             while (window.IsOpen) // main loop
             {
                 window.DispatchEvents();  // sending window events
+                System.Threading.Thread.Sleep(1); // to stop the thread of 1 millisecond, to reduce the load on the processor
+            }
+            window.Dispose(); //remove the window
+        }
+
+        private static void Window_KeyReleased(object sender, SFML.Window.KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
+        {
+            SFML.Graphics.RenderWindow window = (SFML.Graphics.RenderWindow)sender; // the conversion of the sender of the event in the window class
+
+            if (e.Code == SFML.Window.Keyboard.Key.Escape) // close the window if the Escape key is pressed
+            {
+                window.Close(); // close the window
             }
         }
 
